25e558eebabce24ad9b009bd96e90f7931c9d82f
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-02-08 12:28:12 +0100

    test 2 and 3

--- test_hangman_with_hint.py
+++ test_hangman_with_hint.py
@@ -6,3 +6,17 @@ def test_match_with_gaps():
     assert match_with_gaps('a_ _ _e', 'apple')
     assert match_with_gaps('a_ _ l_', 'apple')
     assert match_with_gaps('_ _ _l_', 'apple')
+
+def test_match_with_gaps_wrong_length():
+    assert not match_with_gaps('a _ le', 'apple')
+    assert not match_with_gaps('d_ og', 'dog')
+    assert not match_with_gaps('su_', 'sunny')
+    assert not match_with_gaps('_able', 'able')
+    assert not match_with_gaps('t_ _ e', 'tan')
+
+def test_match_with_gaps_double_letters():
+    assert not match_with_gaps('a _ ple', 'apple')
+    assert not match_with_gaps('su_ _ e_s', 'success')
+    assert match_with_gaps('su _ _ ess', 'success')
+    assert not match_with_gaps('g_ _ l', 'gull')
+    assert match_with_gaps('g _ ll', 'gull')
